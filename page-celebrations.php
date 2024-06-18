7dcdd957d305ff784b07c2d566c6a26ca51c0e6e
Lawrence <lthomas@administrators-iMac.local>
2024-06-18 09:25:10 -0400

    updates to celebrations page

---
+++ page-celebrations.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="content">
+
+<div class="c-celebrations">
+
+<h1 class="c-celebrations__title"><?php echo get_the_title(); ?></h1>
+
+<div class="c-celebrations__content">
+    <?php echo the_content(); ?>
+</div>
+
+
+    </div>
+</div>
+
+<?php get_header(); ?>
