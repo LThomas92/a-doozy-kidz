9cbdb5d728eeff4ec83c9f9040ece73f145215c9
Lawrence Thomas <lawrence@lawrences-mbp-2.mynetworksettings.com>
2025-03-10 23:14:04 -0400

    updates to titles

--- page-daily-schedule.php
+++ page-daily-schedule.php
@@ -4,6 +4,8 @@
 
 <div class="c-daily-schedule">
 
+<h2 class="c-daily-schedule__title"><?php echo the_title();?></h2>
+
 <div class="c-daily-schedule__main-content">
         <?php echo the_content(); ?>
     </div>
--- page-weekly-themes.php
+++ page-weekly-themes.php
@@ -4,6 +4,8 @@
 
 <div class="c-weekly-themes">
 
+<h2 class="c-weekly-themes__title"><?php echo the_title();?></h2>
+
 <div class="c-weekly-themes__main-content">
         <?php echo the_content(); ?>
     </div>
